0747326aea2385f52719a670f200f3592966c7e6
saikrishnamohan318 <saikrishnamohan.kolla@gmail.com>
2023-03-15 16:43:03 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,11 +3,9 @@ var mongoose = require('mongoose');
 var cors = require('cors');
 var emailRouter = require('./routes/email.router');
 var app = express();
-
-const corsOptions = {
-    origin: ['https://sl-back-end.vercel.app/email/emailcheck', 'https://sl-back-end.vercel.app/email/sendotp', 'https://sl-back-end.vercel.app/email/saveEmail', 'https://sl-back-end.vercel.app/email/compareOTP', 'https://sl-back-end.vercel.app/data/saveData']
-};
-app.use(cors(corsOptions));
+app.use(cors({
+    origin: 'http://localhost:3000'
+}));  
 app.use(express.json());
 
 app.use('/email', emailRouter);
